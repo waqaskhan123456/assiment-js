0e6e57ad4b36efbb9555bf5aff81b5e30cb4dc46
waqaskhan <king03428847@gmail.com>
2024-01-30 08:56:11 -0800

    lol

---
+++ app.js
@@ -0,0 +1,87 @@
+console.log("1st ")
+let Country = prompt("ENTER COUNTRY NAME");
+let Population = prompt("ENTER POPULATION");
+let Continent = prompt("ENTER CONTINENT NAME");
+let khan = `THIS MY COUNTRY= ${Country}`;
+let Jan = `THIS is population= ${Population}`;
+let Ali = `THIS is continent= ${Continent}`;
+console.log(khan, Jan, Ali);
+console.log("2nd ")
+let pakistan3 = true
+console.log(pakistan3)
+
+
+console.log("3rd ")
+
+let Waqas = "pakistan"
+var Saad = "population"
+const Wahab = " country"
+console.log(Waqas, Saad, Wahab)
+
+
+console.log("4th ")
+
+let Language = ["ENGLISH", "URDU", "ARABI", "ITALIC", "KOREAYA", "CHINEE", "SPANISH", "TURKEY", "PUNJABI", "CHATRALI", ]
+console.log(" my Language is ", Language[1])
+
+console.log("5th ")
+
+let Population1 = prompt("enter your contry population")
+console.log(Population1 / 2)
+console.log("6th ")
+
+
+let jawad = prompt("enter population")
+let waqar = (jawad + 1)
+console.log(waqar)
+
+
+const country8 = 'YourCountry';
+const continent8 = 'YourContinent';
+let population8 = 100;
+const isIsland8 = false;
+let language8;
+console.log(country8, continent8, population8, isIsland8, language8)
+
+let population9 = 6000000
+let pakistan0 = 996096
+if (population9 < pakistan0) {
+    console.log("yes")
+} else {
+    console.log("no")
+}
+
+
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+console.log(description)
+
+let populationMyCountry = prompt("enter population")
+let india = 33000000
+if (populationMyCountry > india) {
+    console.log("'Portugal's population is 22 million below average'")
+} else {
+    console.log("'Portugal's population is above average'")
+}
+
+let nine = "9" - "5"; // '9' - '5';
+let thirteen = "19" - "13" + "17" // '19' - '13' + '17';
+let ninteen = "19" - "13" + 17 // '19' - '13' + 17;
+let fiftyseven = "123" < 57 // '123' < 57;
+let six = 5 + 6 + "4" + 9 - 4 - 2 // 5 + 6 + '4' + 9 - 4 - 2;
+console.log(nine, thirteen, ninteen, fiftyseven, six)
+
+let sarah = prompt("entre population")
+let peopel = 5000000
+let island0 = prompt("no")
+if (sarah > peopel && island0 == "no") {
+
+    console.log('You should live in Portugal :)')
+
+
+} else if (sarah > peopel) {
+    console.log("unless you live inCanada: ")
+
+} else {
+    console.log('Portugal does not meet your criteria :(')
+}
